e1b605fe85a68fe04c466ee4681638e6f96a0381
desmondbros <62600132+desmondbros@users.noreply.github.com>
2024-01-24 21:35:17 +0000

    24Jan24 CSS

--- js/fadein.js
+++ js/fadein.js
@@ -1,7 +1,7 @@
 $(document).on("scroll", function () {
     var pageTop = $(document).scrollTop()
     var pageBottom = pageTop + $(window).height()
-    var tags = $("section")
+    var tags = $("section, .fadeIn, .fadeLeft, .fadeRight")
     
     for (var i = 0; i < tags.length; i++) {
     var tag = tags[i]
